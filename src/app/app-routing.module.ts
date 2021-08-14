d57b2917bdadc9ef00d0dc7d36dcd5dacbbe0916
Vasif <vasifm1@gmail.com>
2021-08-14 14:52:50 +0530

    craeted item section and add item

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { IteemAddComponent } from './iteem-add/iteem-add.component';
+import { ItemSectionComponent } from './item-section/item-section.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+
+    path:'',
+    component: AppComponent,
+    children:[
+      {
+        path: '',
+        redirectTo:'add-item',
+        pathMatch:'full'
+      },
+      {
+        path:'add-section',
+    component: ItemSectionComponent,
+      },
+      {
+        path:'add-item',
+    component: IteemAddComponent,
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,13 @@ import { UpdateStockComponent } from './update-stock/update-stock.component';
 import { TableConfigComponent } from './table-config/table-config.component';
 import { AddMenuComponent } from './add-menu/add-menu.component';
 import { TakeOrderComponent } from './take-order/take-order.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
+import { MatOptionModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -22,7 +29,13 @@ import { TakeOrderComponent } from './take-order/take-order.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatOptionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
