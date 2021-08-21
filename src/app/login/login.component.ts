06260b3d42f9a51aa915a5ed63067bd1341d3a37
sabithppntr@gmail.com <sabithppntr@gmail.com>
2021-08-21 17:59:21 +0530

    completed waiter oder component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+imageUrl="https://www.logomaker.com/wp-content/uploads/2019/03/icon_only-300x297.png";
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
